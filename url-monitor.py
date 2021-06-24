718362a77eb6755871e0b4183626c08ff2d92696
tomisrd <tom.maurin30100@gmail.com>
2021-06-24 17:05:44 +0000

    init url-monitor.py

---
+++ url-monitor.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# Description : url-monitor.py is a simple script that monitors the HTTP code of a passed website
+# HTTP codes verified -> 200, 401, 403, 404
+# Author: tomisrd
