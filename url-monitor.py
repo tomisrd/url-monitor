67d105090ac6a982a6bada2bf53253e938e43bbf
tomisrd <tom.maurin30100@gmail.com>
2021-06-28 15:32:07 +0000

    requests lib and 200,301,404,.... error handling

--- url-monitor.py
+++ url-monitor.py
@@ -7,8 +7,11 @@
 
 import requests
 
+# creating my list
+my_list_of_website= ['http://google.com/','https://www.youtube.com/','https://twitter.com/login','https://www.twitch.tv/']
+
 # url definition
-url = "http://google.com/blaas"
+url = "http://google.com"
 
 # request url and get status code
 r=requests.head(url)
@@ -21,6 +24,16 @@ if status_code == 200:
 # 301
 elif status_code == 301:
 	print(url + " is redirected - " + str(status_code))
+# 302
+elif status_code == 302:
+	printf(url + " is found - " + str(status_code))
+# 401
+elif status_code == 401:
+	print(url + " is unauthorized - " + str(status_code))
+# 403
+elif status_code == 403:
+	printf(url + " is forbidden - " +str(status_code))
 # 404
 elif status_code == 404:
 	print(url + " is down - " + str(status_code))
+
