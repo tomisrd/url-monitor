8e8f4ee0fc7ff1266f3dae517270a563c2c4a146
tomisrd <tom.maurin30100@gmail.com>
2021-06-29 16:32:53 +0000

    adding url_count_letters and check_url_http_status_code

--- url-monitor.py
+++ url-monitor.py
@@ -7,33 +7,43 @@
 
 import requests
 
-# creating my list
-my_list_of_website= ['http://google.com/','https://www.youtube.com/','https://twitter.com/login','https://www.twitch.tv/']
 
-# url definition
-url = "http://google.com"
+def count_url_letters(url):
+	print(len(url))
 
-# request url and get status code
-r=requests.head(url)
-status_code = r.status_code
 
-# request status code check
-# 200
-if status_code == 200:
-	print(url + " is up - " + str(status_code))
-# 301
-elif status_code == 301:
-	print(url + " is redirected - " + str(status_code))
-# 302
-elif status_code == 302:
-	printf(url + " is found - " + str(status_code))
-# 401
-elif status_code == 401:
-	print(url + " is unauthorized - " + str(status_code))
-# 403
-elif status_code == 403:
-	printf(url + " is forbidden - " +str(status_code))
-# 404
-elif status_code == 404:
-	print(url + " is down - " + str(status_code))
+
+def check_url_http_status_code(url):
+
+	# Request url and get status code
+	r=requests.head(url)
+	status_code = r.status_code
+
+	# Request status code check
+	# 200
+	if status_code == 200:
+		print(url + " is up - " + str(status_code))
+	# 301
+	elif status_code == 301:
+		print(url + " is redirected - " + str(status_code))
+	# 302
+	elif status_code == 302:
+		print(url + " is found - " + str(status_code))
+	# 401
+	elif status_code == 401:
+		print(url + " is unauthorized - " + str(status_code))
+	# 403
+	elif status_code == 403:
+		print(url + " is forbidden - " +str(status_code))
+	# 404
+	elif status_code == 404:
+		print(url + " is down - " + str(status_code))
+
+# Creating my list
+my_list_of_website= ['https://google.com/','https://www.youtube.com/','https://twitter.com/login','https://www.twitch.tv/']
+
+# Loop through my_list_of_website 
+for urls in my_list_of_website:
+	check_url_http_status_code(urls)
+	count_url_letters(urls)
 
